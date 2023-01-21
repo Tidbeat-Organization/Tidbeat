9e9d8085ab216f6e7d5396ce3766f5bf0d2fcc6f
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-01-21 13:16:49 +0000

    Revert "Setup for the login and register programming"
    This reverts commit 1a02ef78088b4bbccfa57edde730789bfc5c3579.

--- Tidbeat/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Tidbeat.Models {
-    public class ApplicationUser : IdentityUser {
-        [PersonalData]
-        public string FullName { get; set; }
-    }
-}
