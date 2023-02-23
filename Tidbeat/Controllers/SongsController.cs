2f437fe181d80f9d713cfe8af3680c2db0c3eb2e
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-02-23 17:40:51 +0000

    more progresses

---
+++ Tidbeat/Controllers/SongsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tidbeat.Data;
+using Tidbeat.Models;
+using Tidbeat.Services;
+
+namespace Tidbeat.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ISpotifyService _spotifyService;
+
+        public SongsController(ApplicationDbContext context, ISpotifyService spotifyService)
+        {
+            _context = context;
+            _spotifyService = spotifyService;
+        }
+
+        // GET: Songs
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Songs.ToListAsync());
+        }
+
+        // GET: Songs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _spotifyService.GetSongAsync(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+    }
+}
