682dc80f0142bec582aaf0683b5646eee7003178
PedroMFC99 <73071815+PedroMFC99@users.noreply.github.com>
2023-02-28 16:25:25 +0000

    Some fixes.

--- Tidbeat/Models/Post.cs
+++ Tidbeat/Models/Post.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Tidbeat.Models {
@@ -7,9 +8,11 @@ namespace Tidbeat.Models {
         public int PostId { get; set; }
 
         [Required]
+        [DisplayName("Título")]
         public string Title { get; set; }
 
         [Required]
+        [DisplayName("Conteúdo")]
         public string Content { get; set; }
 
         public Song? Song { get; set; }
