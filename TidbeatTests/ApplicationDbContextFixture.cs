832682aa4b016608cb1c0f2308427ade30a926c7
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-03-12 16:41:12 +0000

    MusicService tests setup

---
+++ TidbeatTests/ApplicationDbContextFixture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tidbeat.Data;
+
+namespace TidbeatTests {
+    public class ApplicationDbContextFixture {
+
+        public ApplicationDbContext ApplicationDbContext { get; set; }
+
+        public ApplicationDbContextFixture() {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+            ApplicationDbContext = new ApplicationDbContext(options);
+
+            ApplicationDbContext.Database.EnsureCreated();
+        }
+    }
+}
--- TidbeatTests/UnitTest1.cs
+++
@@ -1,8 +0,0 @@
-namespace TidbeatTests {
-    public class UnitTest1 {
-        [Fact]
-        public void Test1() {
-
-        }
-    }
-}
