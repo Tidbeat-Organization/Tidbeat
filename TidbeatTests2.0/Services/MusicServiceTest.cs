a3eec62112e4644a9b1b24823ddd4d2eb3dc3841
Rafael <200221055@estudantes.ips.pt>
2023-03-13 16:53:50 +0000

    testeDone

--- TidbeatTests2.0/Services/MusicServiceTest.cs
+++ TidbeatTests2.0/Services/MusicServiceTest.cs
@@ -1,6 +1,11 @@
+using Castle.Core.Configuration;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Configuration;
+using Moq;
 using SpotifyAPI.Web;
 using Tidbeat.Data;
 using Tidbeat.Services;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace TidbeatTests2._0.Services
 {
@@ -78,35 +83,29 @@ namespace TidbeatTests2._0.Services
 
 
         [Fact]
-        public void GetBandMusicServiceTest()
+        public async void GetBandMusicServiceTest()
         {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                    {"SpotifyAPI:ClientId", "b7e13ced889d41389c8f93767c3803d3"},
+                    {"SpotifyAPI:ClientSecret", "d82a544b10394f07b3ce80c10729f0a4"}
+                     })
+                .Build();
+            //IConfiguration config = Mock.Of<IConfiguration>();
+            //config["SpotifyAPI:ClientId"] = "b7e13ced889d41389c8f93767c3803d3";
+            //config["SpotifyAPI:ClientSecret"] = "d82a544b10394f07b3ce80c10729f0a4";
+            var apiSpotify = new SpotifyService(config);
             var musicService = new MusicService(_context);
-
-            var fullArtist = new FullArtist()
-            {
-                Id = "66CXWjxzNUsdJxJ2JdwvnR",
-                Name = "Ariana Grande",
-                Images = new List<Image> {
-                    new Image() {
-                        Url = "https://i.scdn.co/image/ab6761610000e5ebcdce7620dc940db079bf4952"
-                    }
-                }
-            };
+            var fullArtist = await apiSpotify.GetBandAsync("66CXWjxzNUsdJxJ2JdwvnR");
             musicService.SaveBand(fullArtist);
-
-            var fullTrack = new FullTrack
-            {
-                Id = "6ocbgoVGwYJhOv1GgI9NsF",
-                Name = "7 rings",
-                Artists = new List<SimpleArtist> {
-                    new SimpleArtist {
-                        Id = "66CXWjxzNUsdJxJ2JdwvnR",
-                        Name = "Ariana Grande"
-                    }
-                }
-            };
-
+            var fullTrack = await apiSpotify.GetSongAsync("6ocbgoVGwYJhOv1GgI9NsF");
             musicService.SaveSong(fullTrack);
+            var savedBand = _context.Bands.FirstOrDefault(b => b.BandId == "66CXWjxzNUsdJxJ2JdwvnR");
+            Assert.NotNull(savedBand);
+            Assert.Equal("66CXWjxzNUsdJxJ2JdwvnR", savedBand.BandId);
+            Assert.Equal("Ariana Grande", savedBand.Name);
+            Assert.Equal("https://i.scdn.co/image/ab6761610000e5ebcdce7620dc940db079bf4952", savedBand.Image);
             var savedSong = _context.Songs.FirstOrDefault(s => s.Name == "7 rings");
             Assert.NotNull(savedSong);
             Assert.Equal("7 rings", savedSong.Name);
--- TidbeatTests2.0/Services/SongControllerTest.cs
+++ TidbeatTests2.0/Services/SongControllerTest.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Controller;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Tidbeat.Controllers;
@@ -50,21 +52,34 @@ namespace TidbeatTests2._0.Services
         }
 
         [Fact]
-        public async void DetailsBandsControllerTest()
+        public async Task DetailsBandsControllerTest()
         {
             // Arrange
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "user-id"),
+                new Claim(ClaimTypes.Name, "user-name"),
+                // Add other claims as necessary
+            };
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            var principal = new ClaimsPrincipal(identity);
             var controller = new SongsController(_context, _spotify, _userManager);
             var httpContext = new DefaultHttpContext();
+            httpContext.User = principal;
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+            controller.ControllerContext = controllerContext;
             var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
             controller.TempData = tempData;
-
 
             // Act
             var result = await controller.Details("6ocbgoVGwYJhOv1GgI9NsF");
 
             // Assert
             var viewResult = Assert.IsAssignableFrom<IActionResult>(result);
-
         }
+
     }
 }
