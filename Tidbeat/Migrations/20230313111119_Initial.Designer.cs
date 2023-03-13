23b289a767b82f8f30872d4b30096c27c636d2d7
PedroMFC99 <73071815+PedroMFC99@users.noreply.github.com>
2023-03-13 11:28:15 +0000

    Layout update

--- Tidbeat/Migrations/20230313111119_Initial.Designer.cs
+++ Tidbeat/Migrations/20230313111119_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Tidbeat.Data;
 namespace Tidbeat.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230313095322_Initial")]
+    [Migration("20230313111119_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
