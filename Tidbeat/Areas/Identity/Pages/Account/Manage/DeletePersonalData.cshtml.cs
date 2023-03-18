5dfbeb270a56fb6b6a648eae4626895c43780ef7
PedroMFC99 <73071815+PedroMFC99@users.noreply.github.com>
2023-03-18 17:16:59 +0000

    Error message fix

--- Tidbeat/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
+++ Tidbeat/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
@@ -82,7 +82,7 @@ namespace Tidbeat.Areas.Identity.Pages.Account.Manage
             {
                 if (!await _userManager.CheckPasswordAsync(user, Input.Password))
                 {
-                    ModelState.AddModelError(string.Empty, "Incorrect password.");
+                    ModelState.AddModelError(string.Empty, "A password que inseriu está incorreta.");
                     return Page();
                 }
             }
