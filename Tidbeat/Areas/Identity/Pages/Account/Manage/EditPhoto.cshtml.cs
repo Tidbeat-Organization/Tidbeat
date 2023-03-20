c9f3a608a5aabc9f74728dd43076502cd0efac42
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-03-20 13:30:04 +0000

    can now add and delete photos
    Im not fully removing the photo from the project yet

--- Tidbeat/Areas/Identity/Pages/Account/Manage/EditPhoto.cshtml.cs
+++ Tidbeat/Areas/Identity/Pages/Account/Manage/EditPhoto.cshtml.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tidbeat.Models;
 
 namespace Tidbeat.Areas.Identity.Pages.Account.Manage {
     public class EditPhotoModel : PageModel {
-        public void OnGet() {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ApplicationUser ApplicationUser { get; private set; }
+        public EditPhotoModel(UserManager<ApplicationUser> userManager) {
+            _userManager = userManager;
+        }
 
+        public async Task<IActionResult> OnGetAsync() {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            ApplicationUser = user;
+            return Page();
         }
 
         public async Task<IActionResult> OnPostEditAsync() {
@@ -17,8 +31,9 @@ namespace Tidbeat.Areas.Identity.Pages.Account.Manage {
                     await photoFile.CopyToAsync(stream);
                 }
 
-                // Update the photo with the new file name
-                // logic to update the photo with the new file name goes here
+                var currentUser = await _userManager.GetUserAsync(User);
+                currentUser.ImagePath = "\\" + Path.Combine("images", "user_images", fileName);
+                await _userManager.UpdateAsync(currentUser);
             }
 
             // Redirect back to the index page
@@ -26,8 +41,10 @@ namespace Tidbeat.Areas.Identity.Pages.Account.Manage {
         }
 
 
-        public IActionResult OnPostDelete() {
-            // logic to delete the photo goes here
+        public async Task<IActionResult> OnPostDelete() {
+            var currentUser = await _userManager.GetUserAsync(User);
+            currentUser.ImagePath = null;
+            await _userManager.UpdateAsync(currentUser);
             return RedirectToPage("/Photos/Index");
         }
 
@@ -36,11 +53,16 @@ namespace Tidbeat.Areas.Identity.Pages.Account.Manage {
                 return await OnPostEditAsync();
             }
             else if (Request.Form.ContainsKey("deleteButton")) {
-                return OnPostDelete();
+                return await OnPostDelete();
             }
             else {
                 return Page();
             }
+        }
+
+        public async Task<string> GetUserPhotoUrlAsync() {
+            var currentUser = await _userManager.GetUserAsync(User);
+            return currentUser.ImagePath;
         }
     }
 }
