7535e89fef2b53a6148ccc88ab875a15687af0bb
andrelisboa1 <201902856@estudantes.ips.pt>
2023-03-20 19:26:05 +0000

    Alterar campo de música favorita
    Alterado para uma lista de IDs separados por vírgula, com 2 métodos auxiliares para leitura e escrita da mesma.

--- Tidbeat/Models/ApplicationUser.cs
+++ Tidbeat/Models/ApplicationUser.cs
@@ -2,7 +2,8 @@
 using System.Diagnostics.CodeAnalysis;
 
 namespace Tidbeat.Models {
-    public class ApplicationUser : IdentityUser {
+    public class ApplicationUser : IdentityUser
+    {
         [PersonalData]
         public string FullName { get; set; }
 
@@ -12,13 +13,31 @@ namespace Tidbeat.Models {
         [PersonalData]
         public string Gender { get; set; }
 
+        // Comma separated list of song ids
         [PersonalData]
-        public string? FavoriteSongId { get; set; }
+        public string? FavoriteSongIds { get; set; }
 
         [PersonalData]
         public string? AboutMe { get; set; }
 
         [PersonalData]
         public string? ImagePath { get; set; }
+
+        public List<string> DeserializeFavoriteSongIds()
+        {
+            if (FavoriteSongIds == null)
+            {
+                return new List<string>();
+            }
+
+            return FavoriteSongIds.Split(',').ToList();
+        }
+
+        public void SerializeFavoriteSongIds(List<string> songIds)
+        {
+            FavoriteSongIds = string.Join(',', songIds);
+        }
+
+        
     }
 }
