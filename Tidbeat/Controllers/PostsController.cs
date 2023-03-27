4e1d6f5b9c88588ea03bdb2ce8583018a342e935
Rafael <200221055@estudantes.ips.pt>
2023-03-27 13:28:28 +0100

    Update PostsController.cs

--- Tidbeat/Controllers/PostsController.cs
+++ Tidbeat/Controllers/PostsController.cs
@@ -88,6 +88,7 @@ namespace Tidbeat.Controllers
             return View();
         }
 
+
         // POST: Posts/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
