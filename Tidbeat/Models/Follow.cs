91206232010cae1d3755cce6a77df251012b6326
Rafael <200221055@estudantes.ips.pt>
2023-04-03 17:00:00 +0100

    Update Follow.cs

--- Tidbeat/Models/Follow.cs
+++ Tidbeat/Models/Follow.cs
@@ -1,7 +1,10 @@
-﻿namespace Tidbeat.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tidbeat.Models
 {
     public class Follow
     {
+        [Key]
         public int Id { get; set; }
         public ApplicationUser UserAsker { get; set; }
         public ApplicationUser UserFollowed { get; set; }
