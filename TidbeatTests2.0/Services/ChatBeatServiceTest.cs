59260a868d3b4bb7e189e9c4dc239824c0f87f78
Rafael <200221055@estudantes.ips.pt>
2023-04-11 15:22:16 +0100

    Testes moved

--- TidbeatTests2.0/Services/ChatBeatServiceTest.cs
+++ TidbeatTests2.0/Services/ChatBeatServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Moq;
 using System;
@@ -285,60 +286,6 @@ namespace TidbeatTests2._0.Services {
             Assert.Equal(typeof(Conversation), conversation.GetType());
         }
 
-        [Fact]
-        public async Task GetAllConversations_ConversationsService()
-        {
-            var user = new ApplicationUser()
-            {
-                Id = "f1c1c9b9-ea8f-483d-b0a3-cf63085d3b3d",
-                FullName = "User one",
-                BirthdayDate = DateTime.Now,
-                Gender = "male"
-            };
-            var secondUser = new ApplicationUser()
-            {
-                Id = Guid.NewGuid().ToString(),
-                FullName = "User one",
-                BirthdayDate = DateTime.Now,
-                Gender = "male"
-            };
-            var conversation = new Conversation()
-            {
-                Id = "d5e5c5fb-7a57-4a6d-a61d-b9e6b74cd037",
-                StartDate = DateTime.Now,
-                IsGroupConversation = true,
-            };
-            var participant = new Participant()
-            {
-                Conversation = conversation,
-                User = user
-            };
-            _fixture.Users.Add(user);
-            _fixture.Users.Add(secondUser);
-            var resultdb= _fixture.Conversations.Add(conversation);
-            _fixture.Participants.Add(participant);
-            for (int i = 0; i < 30; i++)
-            {
-                var message = new Message
-                {
-                    Text = "Test" + i,
-                    Created = DateTime.Now,
-                    Status = 0,
-                    Conversation = conversation,
-                    User = user
-                };
-                if (i % 3 == 0)
-                {
-                    message.User = secondUser;
-                }
-                _fixture.Messages.Add(message);
-            }
-            _fixture.SaveChanges();
-            var chatBeatService = new ChatBeatService(_fixture, _mockUserManager.Object);
-            Assert.NotNull(resultdb);
-            Assert.Equal(typeof(EntityEntry<Conversation>), resultdb.GetType());
-            Assert.Equal(typeof(Conversation), conversation.GetType());
-        }
 
     }
 }
--- TidbeatTests2.0/Services/ConversationControllerTest.cs
+++ TidbeatTests2.0/Services/ConversationControllerTest.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -93,6 +95,60 @@ namespace TidbeatTests2._0.Services
             Assert.Equal(0, amount);
             Assert.NotEqual(conversation, firstElement);
 
+        }
+        [Fact]
+        public async Task GetAllConversations_ConversationsController()
+        {
+            var user = new ApplicationUser()
+            {
+                Id = "f1c1c9b9-ea8f-483d-b0a3-cf63085d3b3d",
+                FullName = "User one",
+                BirthdayDate = DateTime.Now,
+                Gender = "male"
+            };
+            var secondUser = new ApplicationUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                FullName = "User one",
+                BirthdayDate = DateTime.Now,
+                Gender = "male"
+            };
+            var conversation = new Conversation()
+            {
+                Id = "d5e5c5fb-7a57-4a6d-a61d-b9e6b74cd037",
+                StartDate = DateTime.Now,
+                IsGroupConversation = true,
+            };
+            var participant = new Participant()
+            {
+                Conversation = conversation,
+                User = user
+            };
+            _fixture.Users.Add(user);
+            _fixture.Users.Add(secondUser);
+            var resultdb = _fixture.Conversations.Add(conversation);
+            _fixture.Participants.Add(participant);
+            for (int i = 0; i < 30; i++)
+            {
+                var message = new Message
+                {
+                    Text = "Test" + i,
+                    Created = DateTime.Now,
+                    Status = 0,
+                    Conversation = conversation,
+                    User = user
+                };
+                if (i % 3 == 0)
+                {
+                    message.User = secondUser;
+                }
+                _fixture.Messages.Add(message);
+            }
+            _fixture.SaveChanges();
+            var chatBeatService = new ChatBeatService(_fixture, _mockUserManager.Object);
+            Assert.NotNull(resultdb);
+            Assert.Equal(typeof(EntityEntry<Conversation>), resultdb.GetType());
+            Assert.Equal(typeof(Conversation), conversation.GetType());
         }
     }
 }
