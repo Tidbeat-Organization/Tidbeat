b271c6baa56a12a902115361a56b3974e37434a7
PedroMFC99 <73071815+PedroMFC99@users.noreply.github.com>
2023-04-12 17:29:21 +0100

    User Page finished

--- Tidbeat/Controllers/UsersController.cs
+++ Tidbeat/Controllers/UsersController.cs
@@ -14,10 +14,27 @@ namespace Tidbeat.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string name, string genre, string country)
         {
             var users = await _userManager.Users.ToListAsync();
+
+            // filter by name
+            if (!string.IsNullOrEmpty(name))
+            {
+                name = name.ToLower();
+                users = users.Where(u => u.FullName.ToLower().Contains(name)).ToList();
+            }
+
+            // filter by country
+            if (!string.IsNullOrEmpty(country))
+            {
+                country = country.ToLower();
+                users = users.Where(u => u.Country != null && u.Country.ToLower().Contains(country)).ToList();
+            }
+
             return View(users);
+
         }
+
     }
 }
