99fe9370d3c7935c71728e3d7c02dc1c36f2dc07
PedroMFC99 <73071815+PedroMFC99@users.noreply.github.com>
2023-04-12 17:57:22 +0100

    Missing list of users page done

---
+++ Tidbeat/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tidbeat.Models;
+
+namespace Tidbeat.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string name, string genre, string country)
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            // filter by name
+            if (!string.IsNullOrEmpty(name))
+            {
+                name = name.ToLower();
+                users = users.Where(u => u.FullName.ToLower().Contains(name)).ToList();
+            }
+
+            // filter by country
+            if (!string.IsNullOrEmpty(country))
+            {
+                country = country.ToLower();
+                users = users.Where(u => u.Country != null && u.Country.ToLower().Contains(country)).ToList();
+            }
+
+            return View(users);
+
+        }
+
+    }
+}
