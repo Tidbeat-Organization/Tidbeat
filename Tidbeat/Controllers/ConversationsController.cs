57484774a95ea469df8c09d620f8a715e988e5eb
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-04-26 19:16:45 +0100

    finished unread messages thing

--- Tidbeat/Controllers/ConversationsController.cs
+++ Tidbeat/Controllers/ConversationsController.cs
@@ -53,10 +53,15 @@ namespace Tidbeat.Controllers
             var allCurrentUserConversations = await _context.Participants.Where(p => p.User.Id == currentUser.Id).Select(p => p.Conversation).ToListAsync();
             var allParticipantsInConversations = await _context.Participants.Include(p => p.User).Where(p => allCurrentUserConversations.Contains(p.Conversation) && p.User != currentUser).ToListAsync();
 
-            var conversationsParticipantsPairs = new List<Tuple<Conversation, List<string>>>();
+            var conversationsParticipantsPairs = new List<Tuple<Conversation, List<string>, int>>();
             foreach (var conversation in allCurrentUserConversations) {
                 var participants = allParticipantsInConversations.Where(p => p.Conversation == conversation).Select(p => p.User.FullName).ToList();
-                conversationsParticipantsPairs.Add(new Tuple<Conversation, List<string>>(conversation, participants));
+                var unreadMessagesCount = await _context.Messages
+                        .Include(m => m.Conversation)
+                        .Include(m => m.User)
+                        .Where(m => m.Conversation.Id == conversation.Id && m.Status == Convert.ToInt32(MessageStatus.Sent) && currentUser.Id != m.User.Id)
+                        .CountAsync();
+                conversationsParticipantsPairs.Add(new Tuple<Conversation, List<string>, int>(conversation, participants, unreadMessagesCount));
             }
             ViewBag.ConversationsParticipantsPairs = conversationsParticipantsPairs;
 
